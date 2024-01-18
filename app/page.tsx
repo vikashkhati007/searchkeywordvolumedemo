b52b9cdede0afba7978cf3f78d37c7b6507f98b7
vikashkhati007 <vikashkhati007@gmail.com>
2024-01-18 08:52:19 +0530

    demo

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,108 @@
-import Image from 'next/image'
+"use client";
+import React, { useState } from "react";
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+// hi, Sir this is a demo of my work on the task you gave me. i can Improve it more if you want.
+//  -  Thank you
+// By -  Vikash Khati
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+interface Props {
+  id: number;
+  keyword: string;
+  searchVolume: number;
+  date?: any;
 }
+
+const Page = () => {
+  //Demo keyword data
+  const [keyword, setKeyword] = useState([
+    { id: 1, keyword: "pen", searchVolume: 0, date: "18/01/2024 07:42" },
+    { id: 2, keyword: "paper", searchVolume: 1, date: "18/01/2024 07:42" },
+  ]);
+
+  //Demo highest search volume
+  const search = keyword.reduce((prev, current) => {
+    return prev.searchVolume > current.searchVolume ? prev : current;
+  }, keyword[0]);
+  const [highestsearchVolume, setHighestsearchVolume] = useState(
+    search.keyword
+  );
+
+  const date = new Date();
+  const dateOptions: Intl.DateTimeFormatOptions = {
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+  };
+  const timeOptions: Intl.DateTimeFormatOptions = {
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: false,
+  };
+
+  const indianDateString = date.toLocaleDateString("en-IN", dateOptions);
+  const indianTimeString = date.toLocaleTimeString("en-IN", timeOptions);
+
+  const formattedDateTime = `${indianDateString} ${indianTimeString}`;
+
+  //Handle search
+  const handleSearch = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setHighestsearchVolume(search.keyword);
+    const searchKeyword = event.currentTarget.product.value;
+    const existingKeyword = keyword.find(
+      (item: Props) => item.keyword === searchKeyword
+    );
+
+    if (existingKeyword) {
+      existingKeyword.searchVolume += 1;
+      existingKeyword.date = formattedDateTime;
+      setKeyword([...keyword]); // Update the state to trigger re-render
+    } else {
+      setKeyword([
+        ...keyword,
+        {
+          id: keyword.length + 1,
+          keyword: searchKeyword,
+          searchVolume: 1,
+          date: formattedDateTime,
+        },
+      ]);
+    }
+  };
+
+  return (
+    <div className="flex flex-col justify-center items-center py-5">
+      <form onSubmit={handleSearch} className="flex gap-2 py-2">
+        <input
+          className=" px-2 py-1 outline-none border border-black rounded-md"
+          name="product"
+          type="text"
+          placeholder="Search products"
+        />
+        <button
+          className="bg-red-500 px-2 py-1 rounded-md text-white border border-black"
+          type="submit"
+        >
+          Search
+        </button>
+      </form>
+
+      <ul className="flex flex-col gap-2">
+        <h1 className="font-semibold text-md text-center bg-gray-500 text-white border rounded-md">
+          BASED ON HIGHER SEARCH VOLUME
+          <p className="bg-red-500">Trending Keyword - {highestsearchVolume}</p>
+        </h1>
+        {keyword
+          .sort((a, b) => b.searchVolume - a.searchVolume) // Sort by searchVolume in descending order
+          .map((product) => (
+            <li className="border px-2 py-2 rounded-md gap-2" key={product.id}>
+              id = {product.id} Keyword= {product.keyword} SearchVolume ={" "}
+              {product.searchVolume} Date = {product.date}
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Page;
